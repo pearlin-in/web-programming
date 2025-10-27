fbd1051391caad91698592bff8afcb0852cadfc0
Joshikaa-Subrayan <js4011@hw.ac.uk>
2025-10-27 11:51:42 +0400

    comp

---
+++ js/welcome.js
@@ -0,0 +1,9 @@
+document.getElementById("signupBtn").addEventListener("click", function() {
+  alert("Redirecting to Sign Up page...");
+  // window.location.href = "signup.html";
+});
+
+document.getElementById("loginBtn").addEventListener("click", function() {
+  alert("Redirecting to Login page...");
+  // window.location.href = "login.html";
+});
